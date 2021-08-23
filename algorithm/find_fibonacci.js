518e5a183c9cdd50334660d6b843ed28eb91152b
ivandi1980 <ivandi.djoh@gmail.com>
2021-08-23 16:10:02 +0800

    refactor find the fibonacci number using memoization in javascript

--- algorithm/find_fibonacci.js
+++ algorithm/find_fibonacci.js
@@ -1,8 +1,23 @@
-function fib(n) {
-  let phi = (1 + Math.sqrt(5)) / 2;
-  let asymp = Math.pow(phi, n) / Math.sqrt(5);
+// function fib(n) {
+//   let phi = (1 + Math.sqrt(5)) / 2;
+//   let asymp = Math.pow(phi, n) / Math.sqrt(5);
 
-  return Math.round(asymp);
+//   return Math.round(asymp);
+// }
+
+// lets use memoization
+
+function fib(n, caches) {
+  caches = caches || [];
+
+  if (caches[n]) {
+    return caches[n];
+  } else {
+    if (n < 3) return 1;
+    else {
+      caches[n] = fib(n - 1, caches) + fib(n - 2, caches);
+    }
+  }
+  return caches[n];
 }
-
-console.log(fib(5));
+console.log(fib(1001));
