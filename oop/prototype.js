aa91374710eb042a01424aa4bb3d3d954ebbe65b
ivandi1980 <ivandi.djoh@gmail.com>
2021-08-25 18:18:48 +0800

    added prototype into OOP topics

---
+++ oop/prototype.js
@@ -0,0 +1,26 @@
+// constructor is just a function
+// Now we can added a Prototype Method into it
+function Book(title, author, year) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+}
+
+// Create a getSummary Prototype
+Book.prototype.getSummary = function () {
+  return `${this.title} was written by ${this.author} in ${this.year}`;
+};
+
+// Get how long this book already written
+Book.prototype.getYearOfBookWritten = function () {
+  const longWrittenBook = new Date().getFullYear() - this.year;
+
+  return `${this.title} is ${longWrittenBook} years old now since it was written by ${this.author}`;
+};
+
+// then Instatiate the Book constructor
+const book1 = new Book("Book1", "John Doe", "1940");
+
+// print out to the console
+console.log(book1.getSummary());
+console.log(book1.getYearOfBookWritten());
