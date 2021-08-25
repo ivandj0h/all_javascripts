c8deb30a5641956438823647aab8666f64627d3b
ivandi1980 <ivandi.djoh@gmail.com>
2021-08-25 18:36:53 +0800

    add function revise on prototype

--- oop/prototype.js
+++ oop/prototype.js
@@ -15,12 +15,20 @@ Book.prototype.getSummary = function () {
 Book.prototype.getYearOfBookWritten = function () {
   const longWrittenBook = new Date().getFullYear() - this.year;
 
-  return `${this.title} is ${longWrittenBook} years old now since it was written by ${this.author}`;
+  return `${this.title} is ${longWrittenBook} years old now since it was written by ${this.author} and not been Revise Yet`;
 };
 
+// Manipulate the year
+Book.prototype.revise = function (newYear) {
+  this.year = newYear;
+  this.revise = true;
+};
 // then Instatiate the Book constructor
 const book1 = new Book("Book1", "John Doe", "1940");
 
 // print out to the console
 console.log(book1.getSummary());
 console.log(book1.getYearOfBookWritten());
+
+book1.revise(2021);
+console.log(book1);
