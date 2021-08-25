09213ac49fb910dcd154cd856494a083363baa32
ivandi1980 <ivandi.djoh@gmail.com>
2021-08-25 17:45:17 +0800

    grouping same values in arrays

--- algorithm/groub_same_values_array.js
+++ algorithm/groub_same_values_array.js
@@ -1,4 +1,4 @@
-const arr = [1, 7, 4, 3, 7, 1].sort((a, b) => a - b),
+const arr = [1, 7, 4, 3, 7, 1, 2, 2, 7].sort((a, b) => a - b),
   grouped = arr.reduce((r, v, i, a) => {
     if (v === a[i - 1]) {
       r[r.length - 1].push(v);
