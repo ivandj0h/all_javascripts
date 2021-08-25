6203c980b41a98fca405c444febc899bc08cc2e4
ivandi1980 <ivandi.djoh@gmail.com>
2021-08-25 17:41:58 +0800

    added OOP concepts

--- oop/index.js
+++ oop/index.js
@@ -1,4 +1,18 @@
 // Object Literals
+const book1 = {
+  // create the objects
+  title: "Book One",
+  author: "John Doe",
+  year: "2021",
+
+  // we can actually add a function in this Object Literals
+  getSumarry: function () {
+    return `${this.title} was written by ${this.author}`;
+  },
+};
+
+// now we can printed out
+console.log(book1.getSumarry());
 
 // ES5 Constructor function & Prototype
 
